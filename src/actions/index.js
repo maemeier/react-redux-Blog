394d6b036b1cc347fa39c7544a06854152e10a55
mae meier <maemeier@live.com>
2019-11-28 11:58:01 +0100

    call api

---
+++ src/actions/index.js
@@ -0,0 +1,9 @@
+import jsonplaceholder from "../apis/jsonPlaceHolder";
+
+export const fetchPosts = async () => {
+  const response = await jsonPlaceHolder.get("/posts");
+  return {
+    type: "FETCH_POSTS",
+    payload: response
+  };
+};
